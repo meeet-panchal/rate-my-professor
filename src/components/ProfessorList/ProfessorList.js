2e3fe314bd6778ba0236d3555a3961f706722e22
Anoop Venkatesh <avenkatesh0864@conestogac.on.ca>
2022-11-26 23:45:05 -0500

    created the professors list page

--- src/components/ProfessorList/ProfessorList.js
+++ src/components/ProfessorList/ProfessorList.js
@@ -7,8 +7,10 @@ import { Progress } from "antd";
 import { Button, Descriptions, PageHeader, Statistic, Tag } from "antd";
 import DescriptionsItem from "antd/lib/descriptions/Item";
 import MainHeader from "../MainHeader";
+import { Divider } from "antd";
+import "./ProfessorList.css";
 
-const ProfessorList = () => (
+const Professor = () => (
   <>
     <nav>
       <div className="logo">
@@ -22,77 +24,48 @@ const ProfessorList = () => (
     </nav>
 
     <Container>
-      <Row
-        className="site-card-border-less-wrapper"
-        style={{ backgroundColor: "#dcdcdc", padding: "20px" }}
-      >
-        <Col xs={12} md={3}>
+      <Row className="text-center">
+        <Col
+          xs={12}
+          md={3}
+          style={{
+            backgroundColor: "rgba(228, 175, 43, 0.88)",
+            padding: "20px",
+          }}
+        >
           {" "}
-          <Card title="Jonathon" bordered={false}>
-            <p>Overall Rating</p>
-            <Progress type="circle" percent={70} width={80} />
+          <Card className="ant-card-head py-0" title="Quality" bordered={false}>
+            <Statistic title="Overall Rating" value={93} suffix="/ 100" />
           </Card>
         </Col>
-
-        <Col xs={12} md={9}>
-          <PageHeader
-            className="site-page-header p-0"
-            title="832474"
-            subTitle="Average"
-            extra={[
-              <Button key="1" type="primary">
-                27th, Nov 2022
-              </Button>,
-            ]}
-          >
-            <Descriptions size="small" column={3}>
-              <Descriptions.Item label="University">
-                University of Waterloo
-              </Descriptions.Item>
-              <Descriptions.Item label="Attendance">
-                <a>Yes</a>
-              </Descriptions.Item>
-              <Descriptions.Item label="Would you recommend?">
-                Yes
-              </Descriptions.Item>
-              <Descriptions.Item label="Teaching">Proficient</Descriptions.Item>
-              <Descriptions.Item label="Textbook">Yes</Descriptions.Item>
-              <Descriptions.Item label="Credit">Yes</Descriptions.Item>
-              <DescriptionsItem>
-                {" "}
-                <div
-                  style={{
-                    width: 170,
-                  }}
-                >
-                  <Progress percent={90} size="small" />
-                  <Progress percent={50} size="small" status="active" />
-                  <Progress percent={70} size="small" status="exception" />
-                  <Progress percent={100} size="small" />
-                </div>
-              </DescriptionsItem>
-              <Descriptions.Item label="Remarks">
-                Tough grader,Respected,Clear instructions,Clear grading criteria
-              </Descriptions.Item>
-              <Descriptions.Item label="Type of exams">
-                Multiple Choice,True/False,Essay, Written Problems, Group
-                Projects
-              </Descriptions.Item>
-
+        <Col
+          xs={12}
+          md={9}
+          style={{ backgroundColor: "#dcdcdc", padding: "20px" }}
+        >
+          <Row className="site-card-border-less-wrapper">
+            <Descriptions size="small">
               <Descriptions.Item
-                className="rounded p-3"
-                style={{ color: "#e4af2be0", backgroundColor: "#e4af2be0" }}
+                label="Professor"
+                style={{ fontSize: "30px", fontWeight: "bold" }}
               >
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                Jonathon
+              </Descriptions.Item>
+              <Descriptions.Item label="Department">Business</Descriptions.Item>
+              <Descriptions.Item label="University">
+                Conestoga College
+              </Descriptions.Item>
+              <Descriptions.Item label="Would take again">
+                50%
+              </Descriptions.Item>
+              <Descriptions.Item label="Level of difficulty">
+                4.1
               </Descriptions.Item>
             </Descriptions>
-          </PageHeader>
+          </Row>
         </Col>
       </Row>
     </Container>
   </>
 );
-export default ProfessorList;
+export default Professor;
