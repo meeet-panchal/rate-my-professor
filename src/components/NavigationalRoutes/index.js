22bae247fee2fbed04feb90fc0d2cfb15322d8b4
meeet-panchal <mpanchal1023@conestogac.on.ca>
2022-12-04 10:13:31 -0500

    Professor's Flow Init

--- src/components/NavigationalRoutes/index.js
+++ src/components/NavigationalRoutes/index.js
@@ -3,6 +3,7 @@ import Home from "../HomePage/home";
 import Universities from "../Universities/universities";
 import ProfessorList from "../ProfessorList/ProfessorList";
 import Professor from "../ProfessorDetails/ProfessorDetails";
+import Ratings from '../Ratings/ratings'
 import RegisterationForm from "../UserDetails/UserRegisteration";
 import AboutUs from "../About/About";
 import ContactUs from "../Contact/contact";
@@ -18,11 +19,12 @@ const NavigationalRoutes = () => {
             <Route path="/about-us" element={<AboutUs />} />
             <Route path="/donation" element={<Donation />} />
             <Route path="/register" element={<RegisterationForm />} />
+            <Route path="/ratings" element={<Ratings />} />
             <Route element={<ProtectedRoutes />}>
                 <Route element={<StudentRoutes/>}>
                     <Route path="/universities" element={<Universities />} />
-                    <Route path="/professors" element={<Professor />} />
-                    <Route path="/professorlist" element={<ProfessorList />} />
+                    <Route path="/professors" element={<ProfessorList />} />
+                    <Route path="/professorlist" element={<Professor />} />
                 </Route>
             </Route>
             {/* <Route path="/ratings" element={<Ratings />} /> */}
